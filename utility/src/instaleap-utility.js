918846d96de1634db8b96dd53d18df46c17c88bd
AndresCalvoi <andres.calvo@instaleap.io>
2022-12-22 14:02:58 -0800

    feat: create a new utility module

---
+++ utility/src/instaleap-utility.js
@@ -0,0 +1,5 @@
+import "./globalStyle.css";
+// Anything exported from this file is importable by other in-browser modules.
+export function showText(text) {
+  console.log(text + " soy un modulo");
+}
